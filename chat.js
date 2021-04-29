568ab9f7d0fbf9622a50d642db6b19900f26c2ae
Olexandr-Sh <shevtcov07@ukr.net>
2021-04-30 00:59:52 +0300

    experiment with spam, faild

--- chat.js
+++ chat.js
@@ -1,30 +1,69 @@
+
 let body = document.body
 
 let inputElement = document.createElement('input');
 body.appendChild(inputElement);
 inputElement.style.padding = '10px';
 
+
+
 let aElement = document.createElement('a');
 body.appendChild(aElement);
+aElement.style.color = 'white';
+aElement.style.fontSize = '20px';
+aElement.style.paddingLeft = '10px';
 aElement.append('submit');
 
-let paragraphElement = document.createElement('p');
-paragraphElement.append('messagesList:');
-body.appendChild(paragraphElement);
+function messageListFunction() {
+  let paragraphElement = document.createElement('p');
+  paragraphElement.append('messagesList:');
+  body.appendChild(paragraphElement);
+};
+messageListFunction();
+
 let messages = [];
 let message_1 = 0;
 
 aElement.addEventListener('click', getMessage);
 
 function getMessage(parametr) {
-    parametr.preventDefault();
-    let newMessage = document.createElement('div');
-    body.appendChild(newMessage);
-    let inputValue = inputElement.value;
-    inputElement.value = '';
-    // let messageObject = { oneMessage: inputValue };
-    messages.push(inputValue);
-    newMessage.textContent += messages[message_1];
-    message_1++;
+  parametr.preventDefault();
+  let newMessage = document.createElement('div');
+  body.appendChild(newMessage);
+  let inputValue = inputElement.value;
+  inputElement.value = '';
+  // let messageObject = { oneMessage: inputValue };
+  messages.push(inputValue);
+  newMessage.textContent += messages[message_1];
+  // message_1++;
+  messages[messages.length - 1];
 };
+
+aElement.addEventListener('click', chatStart);
+
+function clearMessage(clear) {
+  clear.preventDefault();
+  document.getElementById(clear_Message);
+  let clearMes = setInterval(() => {
+    if (clear_Message === true) {
+      messages.shift();
+    } else {
+      messages.append('Hello spamer!)');
+    };
+  }, 5000);
+};
+
+function chatStart() {
+  // let messagesList = [' ', ' ', ' '];
+  let id = setInterval(() => {
+    createNewMessage(messagesList[message_1]);
+    message_1++;
+    if (message_1 === messagesList.length) {
+      message_1 = 0;
+    }
+  }, 1000);
+  setTimout(() => { clearInterval(id); alert('stop'); }, 5000);
+};
+
+
 
